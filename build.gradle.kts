ad10fcc4a5f48171d66da85b43dc90189c9db692
nathan <nathan@dorkbox.com>
2020-06-02 15:49:32 +0200

    Updated dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ plugins {
 
     id("com.dorkbox.Licensing") version "1.3"
     id("com.dorkbox.VersionUpdate") version "1.6.1"
-    id("com.dorkbox.GradleUtils") version "1.5"
+    id("com.dorkbox.GradleUtils") version "1.6"
 
     kotlin("jvm") version "1.3.72"
 }
