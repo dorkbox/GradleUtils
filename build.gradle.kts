f17ffb5459eb65c6f1b09661cd1c47fe5eb20292
Robinson <robinson@dorkbox.com>
2026-01-28 23:24:14 +0100

    updated deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,8 @@ plugins {
 
     id("com.gradle.plugin-publish") version "2.0.0"
 
-    id("com.dorkbox.GradleUtils") version "4.3.1"
-    id("com.dorkbox.Licensing") version "3.0"
+    id("com.dorkbox.GradleUtils") version "4.4"
+    id("com.dorkbox.Licensing") version "3.1"
     id("com.dorkbox.VersionUpdate") version "3.0"
 
     kotlin("jvm") version "2.3.0"
