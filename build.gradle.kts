4914367694547ba33d3063b1e88efaf7b18419c0
Robinson <robinson@dorkbox.com>
2026-01-30 13:28:11 +0100

    version 4.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ object Extras {
     // set for the project
     const val description = "Gradle Plugin to manage various Gradle tasks, such as updating gradle and dependencies"
     const val group = "com.dorkbox"
-    const val version = "4.5"
+    const val version = "4.6"
 
     // set as project.ext
     const val name = "Gradle Utils"
